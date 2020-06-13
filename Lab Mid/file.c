ee4aac6625a513945c476dd9d4e8f377fc6049d9
smdedar <thededar@gmail.com>
2020-06-13 22:52:33 +0600

    add all code

---
+++ Lab Mid/file.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+    float a123;
+    char a;
+    char b123;
+    char c;
+    int ab[5];
+    int bc[2];
+    int ca[7];
+    int ds[4];
+    for( a = 0; a < 5 ;a++)
+     printf("%d ", a);
+    return 0;
+}
